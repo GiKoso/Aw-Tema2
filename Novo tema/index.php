cd9b9fde9eb7119b275066541a6e85bf37d5d1ec
GiKoso <106410718+GiKoso@users.noreply.github.com>
2022-05-27 19:36:40 -0300

    Add files via upload

---
+++ Novo tema/index.php
@@ -0,0 +1,56 @@
+<?php get_header();?>
+	<div class="row">
+		<div class="col-md-12">
+			<div class="jumbotron">
+				 <h2>Seja bem vindo ao meu primeiro tema</h2>
+			</div>
+		</div>
+	</div>
+    <div class="container">
+    <div class="row">
+            <div class="col-md-7">
+        <img src="<?php echo get_template_directory_uri()?>/assets/images/img1.jpg" class="pri">
+            </div>
+            <div class="col-md-5">
+            <h1>Six of Crows – Sangue e Mentiras </h1>
+            <p>Ketterdam: um centro de comércio internacional onde tudo pode ser adquirido pelo preço certo – e ninguém sabe isso melhor do que o criminoso prodígio, Kaz Brekker. Quando uma missão perigosa e mortal é oferecida a Kaz, com o preço de torná-lo mais rico do que poderia sonhar, ele a aceita. Mas não poderia fazer tudo sozinho, para isso ele precisa de ajuda….</p>
+            </div>
+     </div>
+     </div>
+     <br>
+     <div class="container">
+     <div class="row">
+        <div class="col-md-7">
+        <img src="<?php echo get_template_directory_uri()?>/assets/images/img2.jpeg" class="seg">
+        </div>
+        <div class="col-md-5">
+        <h1> Six of Crows </h1>
+        <p>Conheça os personagens carismaticos da serie.</p>
+        </div>
+      </div>
+      </div>
+      <br>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-7">
+                <img src="<?php echo get_template_directory_uri()?>/assets/images/img3.jpg" class="ter">
+            </div>
+            <div class="col-md-5">
+                <h1>Six of Crows ganha serie na Netflix </h1>
+                <p> O livro  se passa anos após os acontecimentos da mesma. Os personagens são outros e a coisa muda de figura, enquanto o foco da trilogia era em poderes Grishas, na duologia, isso fica em segundo plano e o livro foca na ilha de Ketterdam, mais especificamente em seis jovens que fazem parte direta e indiretamente de uma gangue: Os Dreggs. Eles precisam fazer uma missão e os livros são uma reviravolta atrás da outra. </p>
+            </div>
+        </div>
+    </div>
+      <br>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-7">
+                <img src="<?php echo get_template_directory_uri()?>/assets/images/img4.jpg" class="qua">
+                </div>
+                <div class="col-md-5">
+                  <h1> Primeiro livro da serie é lançado </h1>
+                    <p> Depois de alguns anos de espera a editora Gutenberg finalmente anunciou a publicação no Brasil de Six of Crows – Sangue e Mentiras. O livro é o primeiro de uma série que se passa no mesmo universo da Trilogia Grisha, série de grande sucesso de Leigh Bardugo.. </p>
+                </div>
+            </div>
+        </div>
+<?php get_footer();?>
